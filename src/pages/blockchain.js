eb374862cb7766d408119574b2a46d6a01617a05
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-11-13 15:20:09 -0800

    updated blockchain

--- src/pages/blockchain.js
+++ src/pages/blockchain.js
@@ -9,15 +9,15 @@ import PageHeader from "../components/page-header"
 
 import HeroVideo from "../videos/hero.mp4"
 
-const AI = () => (
+const Blockchain = () => (
   <Layout>
-    <SEO title="AI" />
-    <PageHeader title="AI"/>
+    <SEO title="Blockchain" />
+    <PageHeader title="Blockchain"/>
 <div className="contact flex flex-col justify-center items-center py-24 lg:py-40">
-
+<h1 className="text-center">[Coming soon]</h1>
 </div>
 
   </Layout>
 )
 
-export default AI
+export default Blockchain
